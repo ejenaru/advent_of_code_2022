6fe949a4cf5d2d4adab320270801fa53e767910f
Díaz Fossaty María Irene <n791828@santanderglobaltech.com>
2022-12-03 16:28:23 +0100

    done part 2

--- src/advent03.py
+++ src/advent03.py
@@ -1,7 +1,15 @@
+import sys
 import utils
 
 
-file = utils.get_input()
+example = sys.argv[1:]
+
+if example:
+  file = utils.get_example()
+else:
+	file = utils.get_input()
+
+
 input_list = list(file.read().splitlines())
  
 def part1():
@@ -11,20 +19,8 @@ def part1():
     rucksack = get_rucksack_data(string)
     sum = sum + get_priority(rucksack["object"])
   print('Part 1 ==> {}'.format(sum))
-
-def part2():
-  
-  print('Part 2 ==> {}'.format(1))
- 
- 
- 
- 
- 
- 
 def get_priority(letter):
   return ord(letter) - 96 if letter.islower() else ord(letter) - 38
-  
- 
 def get_rucksack_data(rucksack):
   
   compart_len = int(len(rucksack)/2)
@@ -40,16 +36,21 @@ def get_rucksack_data(rucksack):
   return rucksack_data 
 
 
+def part2():
+  sum = 0
+  for i in range(0, len(input_list), 3):
+    group = [input_list[i], input_list[i + 1], input_list[i + 2]]
+    sum += get_priority(get_badge(group))
+  print('Part 2 ==> {}'.format(sum))
 
+def get_badge(group):
+  return ''.join(set(group[0])&set(group[1])&set(group[2]))
 
 
 def main():
-	part1()
-	part2()
-
-
-
-
+  
+  part1()
+  part2()
 
 
 if __name__ == "__main__":
