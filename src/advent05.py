607a5569023a3b7d388af99401c030331f8c6a8b
Díaz Fossaty María Irene <n791828@santanderglobaltech.com>
2022-12-05 17:08:18 +0100

    part 2 done, litte dirty but whatever

--- src/advent05.py
+++ src/advent05.py
@@ -1,7 +1,6 @@
 import sys
 import utils
 
-
 example = sys.argv[1:]
 
 if example:
@@ -11,7 +10,15 @@ else:
 
 input_list = list(file.read().splitlines())
 
-def get_cargo_list():
+
+
+
+
+
+
+
+
+def get_empty_cargo_list():
   cargo_list = []
   for i in range(0, len(input_list[0]), 4):
     cargo_list.append([]) 
@@ -19,21 +26,21 @@ def get_cargo_list():
 
 
 instructions = []
-cargo_list = get_cargo_list()
+
 
     
-def print_cargo():
+def print_cargo(cargo_list):
   for stack in cargo_list:
     print(cargo_list.index(stack), end = '')
     for item in stack:
       print(item, end = '')
     print()
 
-def turn_cargo():
+def turn_cargo(cargo_list):
   for i in range(len(cargo_list)):
     cargo_list[i].reverse()
 
-def get_initial_stack():
+def get_initial_stack(cargo_list):
   global instructions
 
   for line_raw in input_list:
@@ -41,8 +48,6 @@ def get_initial_stack():
     if line_raw[:3:] == ' 1 ':
       #get instructions out
       instructions = input_list[input_list.index(line_raw) + 2:len(input_list)]
-      #for instruction in input_list[input_list.index(line_raw) + 2:len(input_list)]:
-      #  instructions.append(instruction)
       break
     #Iterate through lines to fill stack (upsidedown)
     line = [line_raw[x:x + 3] for x in range(0, len(line_raw), 4)]
@@ -55,18 +60,12 @@ def get_initial_stack():
       cargo_list[i].append(item)
     #print('DEBUG: ', line)
     #print('ITERATE:  ',  input_list.index(line_raw))
-  turn_cargo()
+  turn_cargo(cargo_list)
   
 
   return cargo_list
 
-
-
-
-
-
-
-def do_instrucctions():
+def do_instrucctions(cargo_list):
   for line in instructions:
 
     ins = line.split(" ")
@@ -77,26 +76,53 @@ def do_instrucctions():
     for i in range(num):
       cargo_list[cargo_to].append(cargo_list[cargo_from].pop())
       
- 
+def do_instructions_2(cargo_list):
+  temp_list = []
+  for line in instructions:
+
+    ins = line.split(" ")
+    num = int(ins[1])
+    cargo_from = int(ins[3]) - 1
+    cargo_to = int(ins[5]) - 1
+    
+    for i in range(num):
+      temp_list.append(cargo_list[cargo_from].pop())
+    temp_list.reverse()
+    cargo_list[cargo_to].extend(temp_list)
+    temp_list.clear()
+
+
 def part1():
+  print("PART_1 ___________________________")
   sum = ''
-  get_initial_stack()
-  print_cargo()
+  cargo_list = get_empty_cargo_list()
+  get_initial_stack(cargo_list)
+  print_cargo(cargo_list)
   print("------------------")
-  do_instrucctions()
-  print_cargo()
+  do_instrucctions(cargo_list)
+  print_cargo(cargo_list)
   print("-------------------")
   
   for cargo in cargo_list:
     sum += cargo.pop()
-  print('Part 1 ==> {}'.format(sum))
+  print('Solution ==> {}'.format(sum))
 
 
 
 def part2():
-  sum = 0
+  print("PART_2_______________________________")
+  
+  cargo_list = get_empty_cargo_list()
+  get_initial_stack(cargo_list)
+  sum=''
+  print_cargo(cargo_list)
+  print("-----------")
+  do_instructions_2(cargo_list)
+  print_cargo(cargo_list)
+  for cargo in cargo_list:
+    sum += cargo.pop()
 
-  print('Part 2 ==> {}'.format(sum))
+  print('Solution ==> {}'.format(sum))
 
 
 
